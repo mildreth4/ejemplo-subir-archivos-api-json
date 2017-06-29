88912ca78bb7321d3f257b4fa97486212d21c3c3
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-06-29 13:11:45 -0300

    Quitando serializado a string del campo portada.

--- frontend/app/models/disco.js
+++ frontend/app/models/disco.js
@@ -4,10 +4,11 @@ import { validatePresence } from "ember-changeset-validations/validators";
 export default DS.Model.extend({
   titulo: DS.attr("string"),
   artista: DS.attr("string"),
-  portada: DS.attr("string"),
+  portada: DS.attr(),
 
   validaciones: {
     titulo: [validatePresence(true)],
-    artista: [validatePresence(true)]
+    artista: [validatePresence(true)],
+    portada: [validatePresence(true)]
   }
 });
